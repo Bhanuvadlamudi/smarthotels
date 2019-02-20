6268ff6c052dac9ba27572957d400e564f22fdcd
Bhanu V <vadlamudi.bhanu@gmail.com>
2019-02-20 17:57:51 -0500

    req

--- req.sh
+++ req.sh
@@ -4,7 +4,9 @@ sudo pip install -U nltk
 pip install --upgrade gensim
 pip install -U spacy
 python -m spacy download en
-
+pip install -U textblob
+python -m pip install -U matplotlib
+sudo apt-get install python-tk
 pip install pyldavis
 pip install bokeh
 
