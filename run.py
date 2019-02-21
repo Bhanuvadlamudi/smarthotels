ed960f4ff8d0983f4ffd9bd25da64acbf6a06302
Bhanu V <vadlamudi.bhanu@gmail.com>
2019-02-21 01:20:32 -0500

    smart hotels

--- run.py
+++ run.py
@@ -2,4 +2,4 @@
 from flaskexample import app
 # from dotenv import load_env
 app.secret_key = 'super secret key'
-app.run(host='0.0.0.0', debug = True)
+app.run(host='0.0.0.0', port=8080, debug = True)
